0b345c48e88c0086d969a499c068538bc5485d27
Ronnie Mathew <ronnie@bricksimple.com>
2017-09-29 09:26:26 -0400

    Implemented the ability to translate strings just like in the angular 1 version of the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,28 +2,47 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { TranslateModule, TranslateLoader, TranslateService } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
-import { InMemoryDataService }  from './in-memory-data.service';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './share/header/header.component';
+import { FooterComponent } from './share/footer/footer.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+    return new TranslateHttpLoader(http, "./languages/", ".json");
+}
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
         HttpModule,
-        InMemoryWebApiModule.forRoot(InMemoryDataService),
+        HttpClientModule,
+        TranslateModule.forRoot({  	
+            loader: {
+              provide: TranslateLoader,
+              useFactory: HttpLoaderFactory,
+              deps: [ HttpClient ]
+            }
+        }),
         AppRoutingModule
     ],
     declarations: [
         AppComponent,
-        HeaderComponent
+        HeaderComponent,
+        FooterComponent
     ],
-    providers: [ ],
+    providers: [ TranslateService ],
     bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+    constructor(private translate: TranslateService) {
+        translate.addLangs([ 'en' ]);
+        translate.setDefaultLang('en');
+        translate.use('en');
+    }
+}
