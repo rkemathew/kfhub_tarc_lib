91c851b2fd14aec0933008fa283afbd9cf3806de
Ronnie Mathew <ronnie@bricksimple.com>
2018-01-30 18:03:09 -0500

    Got the isAuthenticated() function of AuthService working and also added code to prevent the header and footer from being shown for the login, registration and other non-app pages

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 
 import { User } from '../models/user.model';
+import { SessionInfo } from '../models/sessioninfo.model';
 
 const SESSION_TIMEOUT_IN_MILLIS: number = 1200000; // 20 Minutes
 const AUTHTOKEN_REFRESH_TIMEOUT_IN_MILLIS: number = 900000; // 15 Minutes
-const SESSION_STORAGE_LOGGED_IN_USER: string = 'loggedInUser';
+const SESSION_STORAGE_INFO: string = 'sessionInfo';
 
 @Injectable()
 export class AuthService {
@@ -13,19 +14,18 @@ export class AuthService {
     public isAuthenticated(): boolean {
         let retVal: boolean = false;
 
-        const sessionInfoString = sessionStorage.getItem('sessionInfo');
-        const sessionInfo = sessionInfoString ? JSON.parse(sessionInfoString) : null;
-        const authToken = sessionInfo ? sessionInfo.authToken : null;
-        const authTokenRefreshTime = sessionInfo ? sessionInfo.authTokenRefreshTime : null;
-        const loggedInTime = sessionInfo ? sessionInfo.loggedInTime : null;
-        const lastActivityTime = sessionInfo ? sessionInfo.lastActivityTime : null;
+        const sessionInfoString = sessionStorage.getItem(SESSION_STORAGE_INFO);
+        const sessionInfo: SessionInfo = sessionInfoString ? this.getSessionInfo(sessionInfoString) : null;
+        const authTokenRefreshTime: Date = sessionInfo ? sessionInfo.AuthTokenRefreshTime : null;
+        const loggedInTime: Date = sessionInfo ? sessionInfo.LoggedInTime : null;
+        const lastActivityTime: Date = sessionInfo ? sessionInfo.LastActivityTime : null;
 
         const now = new Date().getTime();
-        const lastActivityElapsedTime = now - lastActivityTime;
-        const authTokenElapsedTime = now - authTokenRefreshTime;
+        const lastActivityElapsedTime = lastActivityTime ? now - lastActivityTime.getTime() : -1;
+        const authTokenElapsedTime = authTokenRefreshTime ? now - authTokenRefreshTime.getTime() : -1;
         
-        if (lastActivityElapsedTime < SESSION_TIMEOUT_IN_MILLIS) {
-            if (authTokenElapsedTime < AUTHTOKEN_REFRESH_TIMEOUT_IN_MILLIS) {
+        if (lastActivityElapsedTime > -1 && lastActivityElapsedTime < SESSION_TIMEOUT_IN_MILLIS) {
+            if (authTokenElapsedTime > -1 && authTokenElapsedTime < AUTHTOKEN_REFRESH_TIMEOUT_IN_MILLIS) {
                 retVal = true;
             } else {
                 // Re-validate the AuthToken by issuing a call to UserInfo to ensure that the authToken is still valid
@@ -40,6 +40,16 @@ export class AuthService {
 
     public storeAuthenticationInfo(authInfo: any): void {
         const user: User = new User(authInfo);
-        sessionStorage.setItem(SESSION_STORAGE_LOGGED_IN_USER, JSON.stringify(user));
+        const now = new Date();
+        const sessionInfo: SessionInfo = new SessionInfo(user, now, now, now);
+        sessionStorage.setItem(SESSION_STORAGE_INFO, JSON.stringify(sessionInfo));
+    }
+
+    public removeAuthenticationInfo(): void {
+        sessionStorage.removeItem(SESSION_STORAGE_INFO);
+    }
+
+    private getSessionInfo(sessionInfoString): SessionInfo {
+        return SessionInfo.parseString(sessionInfoString);
     }
 }
