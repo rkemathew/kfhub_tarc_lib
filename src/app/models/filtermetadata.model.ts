b3ba475e764c979d2ffdc1f8f9030991d83681c2
Ronnie Mathew <ronnie@bricksimple.com>
2018-01-29 21:25:20 -0500

    In the process of converting the SpSearch Controller to Angular 4 Component in Typescript

---
+++ src/app/models/filtermetadata.model.ts
@@ -0,0 +1,7 @@
+export class FilterMetadata {
+    constructor(
+        public id: number,
+        public name: string,
+        public searchOn: boolean
+    ) {}
+}
--- src/app/successprofile/spsearch.component.ts
+++ src/app/successprofile/spsearch.component.ts
@@ -1,10 +1,248 @@
 import { Component, OnInit } from '@angular/core';
 
+import { FilterMetadata } from '../models/filtermetadata.model'
+
 @Component({
     selector: 'app-spsearch',
     templateUrl: 'spsearch.component.html',
     styleUrls: [ 'spsearch.component.less' ]
 })
-export class SPSearchComponent {
+export class SPSearchComponent implements OnInit {
+    private page: string = 'successProfileSearch';
+    private searchString: string = '';//$route.current.params.queryString || '';
+    private searchResults: Array<string> = [];
+    private pagingInfo: Object = {};
+    private pageIndex: number = 1;
+    private pageSize: number = 20;
+    private sorting: Array<Object> = [];
+    private subscriptions = null;
+    private selectedFunctionFilters: Array<Object> = [];
+    private selectedLevelFilters: Array<Object> = [];
+    private selectedGradeFilters: Array<Object> = [];
+    private searchLoading: boolean = false;
+    private searchQueueLength: number = 0;
+    private scrollingPageIndex: number = 0;
+    private reSearch: boolean = false;
+    private showFilters: boolean = false;
+    private appliedFilters: Array<Object> = [];
+    private listView: boolean = true;
 
+    ngOnInit() {
+        this.getSubscriptions();
+        this.refreshResults(true);
+        this.checkSearch();
+
+        // let metadata = SearchService.getMetadata('jobDescriptionSearch')
+//        let metadata: FilterMetadata[] = [];
+//        let successProfileMetadata: FilterMetadata[] = metadata.filter((m) => m.name === 'SEARCH_SUCCESS_PROFILES');
+//        let filterMetadata: FilterMetadata[] = successProfileMetadata ? Object.assign({}, successProfileMetadata[0].searchOn) : [];
+
+//        filterMetadata.forEach((f) => {
+//            $scope['all' + this.capitalizeFirstChar(f.name) + 'Filters'] = Object.assign({}, f.options);
+//        });
+
+//        SPShareService.getPrivacyPolicy('PRODUCTS_HUB');
+    }    
+
+    getSubscriptions() {
+//        TAcqProjectService.getAssessmentSubscriptions().then(function(result) {
+//            this.subscriptions = result;
+//        });
+    }
+
+    refreshResults(resetResults){
+        this.searchLoading = true;
+        this.searchQueueLength++;
+        let queuePointer = this.searchQueueLength;
+
+//        SearchService.searchProfile($scope.page, $scope.searchString, $scope.appliedFilters, $scope.sorting, $scope.pageIndex, $scope.pageSize).then(function(response){
+//            this.searchCallback(response, resetResults, queuePointer);
+            this.searchLoading = false;
+//        });
+    };
+
+    searchCallback(response, resetResults, queuePointer) {
+        if (queuePointer < this.searchQueueLength) {
+            return;
+        }
+
+        if (!response) {
+            this.searchResults = [];
+            return;
+        }
+
+        this.pageIndex = 1;
+        if (resetResults){
+            this.searchResults = [];
+            this.scrollingPageIndex = this.pageIndex;
+        }
+
+        response.jobs.forEach((p) => {
+            let date = new Date(parseInt(p.createdDateTime));
+            let monthNamesShort = ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+            p.dateCreated = monthNamesShort[date.getMonth()] + '-' + date.getDate() + ', ' + date.getFullYear();
+    
+            this.searchResults.push(p);
+        });
+
+        this.pagingInfo = Object.assign({}, response.paging);
+    };
+
+    checkSearch() {
+        if (this.reSearch) {
+            this.reSearch = false;
+            this.refreshResults(true);
+        }
+
+        setTimeout(this.checkSearch, 300);
+    };
+
+    capitalizeFirstChar(filterName) {
+        return filterName.toLowerCase().charAt(0).toUpperCase() + filterName.toLowerCase().slice(1);
+    };
+
+    toggleFilters() {
+        this.showFilters = !this.showFilters;
+    };
+
+    addFilter(type, filter) {
+/*
+        var newFilter = {
+            type: type,
+            content: filter
+        };
+
+        let index = this.appliedFilters.findIndex((f) => f.type === type && f.content.name === filter.name);
+        if (index !== -1) {
+            if (index > -1) {
+                this.removeFilter(index, this.appliedFilter);
+            }
+        } else {
+            this.appliedFilters.push(newFilter);
+            console.log("new filter: ", newFilter);
+            newFilter.isFiltered = true;
+        }
+
+        this.refreshResults(true);
+*/
+    };
+
+    isFiltered(type, filter) {
+//        return this.appliedFilters.findIndex((f) => f.type === type && f.content.name === filter.name && f.isFiltered) > -1;
+    }
+
+    removeFilter(index, appliedFilter) {
+/*
+        this.appliedFilters.splice(index, 1);
+
+        switch (appliedFilter.type) {
+            case 'grade':
+                this.selectedGradeFilters = this.selectedGradeFilters.filter((filter) => filter.id !== appliedFilter.content.id);
+                break;
+            case 'level':
+                this.selectedLevelFilters = this.selectedLevelFilters.filter((filter) => filter.id !== appliedFilter.content.id);
+                break;
+            case 'function':
+                this.selectedFunctionFilters = this.selectedFunctionFilters.filter( (filter) => filter.id !== appliedFilter.content.id);
+                break;
+            default:
+                break;
+        };
+
+        this.refreshResults(true);
+*/
+    }
+    
+    removeAllFilter() {
+        this.appliedFilters = [];
+        this.selectedGradeFilters = [];
+        this.selectedLevelFilters = [];
+        this.selectedFunctionFilters = [];
+        this.refreshResults(true);
+    }
+
+    sortResults(colName) {
+/*        
+        let index = this.sorting.map((entry) => entry.sortColumn).indexOf(colName);
+        if (index === -1) {
+            this.sorting.push({
+                sortColumn: colName,
+                sortBy: 'ASC'
+            });
+        } else {
+            console.log(this.sorting[index].sortBy);
+            if (this.sorting[index].sortBy === 'ASC') {
+                this.sorting[index].sortBy = 'DESC';
+            } else if (this.sorting[index].sortBy === 'HIDE') {
+                this.sorting[index].sortBy = 'ASC';
+            } else {
+                this.sorting[index].sortBy = 'HIDE';
+                this.sorting = [];
+                return false;
+            }
+        }
+
+        this.refreshResults(true);
+*/
+    }
+
+    sortingInOrder = function(colName) {
+        var sortObj = this.sorting.find((s) => s.sortColumn === colName);
+        return sortObj ? sortObj.sortBy : false;
+    }
+
+    setFavorite = function(profile) {
+        profile.favorite = !profile.favorite;
+    }
+
+    setListView(listView) {
+        listView = listView;
+    };
+
+    createJobDescription(successProfileId) {
+//        WizardService.createJDFromSP(successProfileId).then(function (res) {
+//            $location.path('/talentarchitect/jobdescription/jobs/new');
+//        })
+    };
+
+    loadMoreResults() {
+/*        
+        if ((this.scrollingPageIndex + 1) > this.pagingInfo.totalPages || !this.searchResults.length) {
+            return;
+        }
+
+        if (this.page === 'successProfileSearch' && !this.searchLoading && $(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+            this.scrollingPageIndex += 1;
+            this.pageIndex = this.scrollingPageIndex;
+            this.refreshResults(false);
+        }
+*/
+    };
+
+    // angular.element($window).on('scroll', loadMoreResults);
+    // $scope.$on('$destroy', function () {
+    //     angular.element($window).off('scroll', loadMoreResults);
+    // });
+
+    // Update the applied filters when the filter selection is changed
+    filtersChanged(type, value) {
+        let filters = value.map((filter) => {
+            return {
+                content: filter,
+                type: type,
+            };
+        });
+
+/*
+        this.appliedFilters = _
+            .chain(this.appliedFilters)
+            .reject(function (filter) {
+                return filter.type === type;
+            })
+            .concat(filters)
+            .value();
+
+        this.refreshResults(true);
+*/
+    };
 }
