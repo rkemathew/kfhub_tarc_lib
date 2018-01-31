54954b3ed344e7ec46239831d457c4bd005403d8
Ronnie Mathew <ronnie@bricksimple.com>
2018-01-31 10:55:06 -0500

    Added methods in the header component for navigation and the drop down menu collapse expand

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     imports: [
         BrowserModule,
         FormsModule,
+        NgbModule.forRoot(),
         HttpModule,
         HttpClientModule,
         TranslateModule.forRoot({
--- src/app/shared/modules/header/header.component.ts
+++ src/app/shared/modules/header/header.component.ts
@@ -14,18 +14,39 @@ const nonAppPages: Array<string> = [
     styleUrls: ['./header.component.less']
 })
 export class HeaderComponent {
+    public isShowMenu: boolean = false;
+
     constructor(
         private router: Router,
         private location: Location,
         private authService: AuthService
     ) {};
 
+    isAppPages(): boolean {
+        return nonAppPages.indexOf(this.location.path()) === -1;
+    }
+
+    toggleMenu(): void {
+        this.isShowMenu = !this.isShowMenu;
+    }
+
+    preferences(): void {
+        this.isShowMenu = false;
+        // TODO:
+    }
+    
     logout(): void {
+        this.isShowMenu = false;
         this.authService.removeAuthenticationInfo();
         this.router.navigate(['/login']);
     }
 
-    isAppPages(): boolean {
-        return nonAppPages.indexOf(this.location.path()) === -1;
+    hasAdminRole(): boolean {
+        return true;
+    }
+
+    redirectToAdminModule(): void {
+        this.isShowMenu = false;
+        // TODO:
     }
 }
